96afb160eb0e7738eb75ecdd7cf5ad75ff7fb3da
Michael <msolano@imaginacolombia.com>
2022-02-04 18:02:42 -0500

    Add: third prop 'params' to method `filterQuery`

--- Icrud/Repositories/Eloquent/EloquentCrudRepository.php
+++ Icrud/Repositories/Eloquent/EloquentCrudRepository.php
@@ -106,7 +106,7 @@ abstract class EloquentCrudRepository extends EloquentBaseRepository implements 
    * @param $query
    * @param $filter
    */
-  public function filterQuery($query, $filter)
+  public function filterQuery($query, $filter, $params)
   {
     return $query;
   }
@@ -280,7 +280,7 @@ abstract class EloquentCrudRepository extends EloquentBaseRepository implements 
       if (is_array($filters)) $filters = (object)$filters;
       $filters->params = $params;
       //Add model filters
-      $query = $this->filterQuery($query, $filters);
+      $query = $this->filterQuery($query, $filters, $params);
     }
 
     //Order Query
