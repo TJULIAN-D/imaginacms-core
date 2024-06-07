e73125cc12d1b754db06607e8c06082d0afe4749
Cristian Marin <marin101998@gmail.com>
2024-06-07 10:41:05 -0500

    +fix | v10.x 07-06-24 migrate and seed modules Core

--- Console/Installers/Scripts/ModuleMigrator.php
+++ Console/Installers/Scripts/ModuleMigrator.php
@@ -7,51 +7,25 @@ use Modules\Core\Console\Installers\SetupScript;
 
 class ModuleMigrator implements SetupScript
 {
-    /**
-     * @var array
-     */
-    protected $modules = [
-        'Isite',
-        'Ifillable',
-        'Ischedulable',
-        'Setting',
-        'Menu',
-        'Media',
-        'Notification',
-        'Page',
-        'Dashboard',
-        'Translation',
-        'Slider',
-        'Tag',
-        'Iblog',
-        'Iforms',
-        'Iprofile',
-        'Ilocations',
-        'Iredirect',
-        'Igamification',
-        'Iqreable',
-        'Ibuilder'
-      
-    ];
-
-    /**Q
-     * Fire the install script
-     *
-     * @return mixed
-     */
-    public function fire(Command $command)
-    {
-        if ($command->option('verbose')) {
-            $command->blockMessage('Migrations', 'Starting the module migrations ...', 'comment');
-        }
-
-        foreach ($this->modules as $module) {
-            if ($command->option('verbose')) {
-                $command->call('module:migrate', ['module' => $module]);
-
-                continue;
-            }
-            $command->callSilent('module:migrate', ['module' => $module]);
-        }
+  /**
+   * Fire the install script
+   *
+   * @return mixed
+   */
+  public function fire(Command $command)
+  {
+    if ($command->option('verbose')) {
+      $command->blockMessage('Migrations', 'Starting the module migrations ...', 'comment');
     }
+
+    $modules = config('asgard.core.config.CoreModules');
+    foreach ($modules as $module) {
+      if ($command->option('verbose')) {
+        $command->call('module:migrate', ['module' => $module]);
+
+        continue;
+      }
+      $command->callSilent('module:migrate', ['module' => $module]);
+    }
+  }
 }
--- Console/Installers/Scripts/ModuleSeeders.php
+++ Console/Installers/Scripts/ModuleSeeders.php
@@ -7,45 +7,26 @@ use Modules\Core\Console\Installers\SetupScript;
 
 class ModuleSeeders implements SetupScript
 {
-    /**
-     * @var array
-     */
-    protected $modules = [
-        'Isite',
-        'Ifillable',
-        'Ischedulable',
-        'Setting',
-        'Page',
-        'Iblog',
-        'Ilocations',
-        'Iprofile',
-        'Isite',
-        'Notification',
-        'Igamification',
-        'Slider',
-        'Iredirect',
-        'Iqreable'
-      
-    ];
 
-    /**
-     * Fire the install script
-     *
-     * @return mixed
-     */
-    public function fire(Command $command)
-    {
-        if ($command->option('verbose')) {
-            $command->blockMessage('Seeds', 'Running the module seeds ...', 'comment');
-        }
-
-        foreach ($this->modules as $module) {
-            if ($command->option('verbose')) {
-                $command->call('module:seed', ['module' => $module]);
-
-                continue;
-            }
-            $command->callSilent('module:seed', ['module' => $module]);
-        }
+  /**
+   * Fire the install script
+   *
+   * @return mixed
+   */
+  public function fire(Command $command)
+  {
+    if ($command->option('verbose')) {
+      $command->blockMessage('Seeds', 'Running the module seeds ...', 'comment');
     }
+
+    $modules = config('asgard.core.config.CoreModules');
+    foreach ($modules as $module) {
+      if ($command->option('verbose')) {
+        $command->call('module:seed', ['module' => $module]);
+
+        continue;
+      }
+      $command->callSilent('module:seed', ['module' => $module]);
+    }
+  }
 }
